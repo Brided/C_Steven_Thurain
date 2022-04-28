b357e45c410fda3754d93bb280e6a2a318ad7066
Steven <ungsteven6@gmail.com>
2022-04-28 14:42:02 +0200

    Début calc_unbounded_int.c
    cherche ou les options sont positionés dans la commande

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 create:
 	gcc -g -Wall -c unbounded_int.c -o unbounded_int.o
 	gcc -g -Wall -c test_unbounded.c -o test_unbounded.o
+	gcc -g -Wall -c calc_unbounded_int.c -o calc_unbounded_int.o
+
 	gcc -g -Wall test_unbounded.o unbounded_int.o -o prog
+	gcc -g -Wall calc_unbounded_int.o unbounded_int.o -o calc_unbounded_int
 
 clean:
 	rm *.o
--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
-#include<unbounded_int.h>
+#include<assert.h>
+#include"unbounded_int.h"
+
+/*
+  Cherche la chaine de charactère égale à searched. Retourne sa position si une telle chaine existe, -1 sinon.
+  Si plusieurs chaines correspondent à searched retourne la première occurence.
+
+                                          "-i"    "-o"
+  Exemples: ./calc_unbounded_int -i    ->   1      -1
+            ./calc_unbounded_int -i -o ->   1       2
+            ./calc_unbounded_int -o    ->  -1       1
+*/
+int search_args_options(int argc, char *argv[], char *searched) {
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], searched) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 int main(int argc, char *argv[]) {
   /* code */
+  int inputArgPos = search_args_options(argc, argv, "-i");
+  int outputArgPos = search_args_options(argc, argv, "-o");
+  printf("%d\n", inputArgPos);
+  printf("%d\n", outputArgPos);
   return 0;
 }
