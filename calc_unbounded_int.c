bfea2492ac3bbace68a4f98f14ca8b32f5bdb77f
NAN HEIN Thurain <thurainnanhein69@gmail.com>
2022-05-03 15:39:08 +0200

    Debut decoupage des mots, et ajout d'un fichier test.

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -5,6 +5,10 @@
 
 #include"unbounded_int.h"
 
+#define NB_MAX_MOTS 20
+#define NB_MAX_LINES 20
+#define NB_CHAR_LINE 1024
+
 /*
   Cherche la chaine de charactère égale à searched. Retourne sa position si une telle chaine existe, -1 sinon.
   Si plusieurs chaines correspondent à searched retourne la première occurence.
@@ -67,12 +71,73 @@ exp* init_exp(){
   return res;
 }
 
+
+
+void print_exp(exp* exp){
+  if(exp==NULL) printf("exp null\n");
+  else printf("exp:\n");
+  cellString* iter=exp->premier;
+  while(iter!=NULL){
+    printf("%s,%d  ",iter->mot,iter->type);
+    iter=iter->suivant;
+  }
+  printf("\n");
+}
+
+
+int est_operation(char* v){
+  if(v==NULL || strlen(v)!=1) return 0;
+  if(*v=='+' || *v=='-' || *v=='*') return 1;
+  if(*v=='=') return 2;
+  else return 0;
+}
+
+
+
+/*
+  Vérifie si la chaîne de caractère est éligible d'être le nom d'une variable ou un entier.
+  Retourne:
+  - 4 si '='
+  - 3 si c'est une opération (+,-,*),
+  - 2 si elle contient des lettres et potentiellement des chiffres,
+  - 1 si elle contient que des chiffres donc elle est un entier, 
+  - 0 pour tout autre.
+*/
+int est_variable_entier(char* v){
+  if(v==NULL || strlen(v)==0) return 0;
+  int op=est_operation(v);
+  if(op==1) return 3;
+  else if(op==2) return 4;
+  int var=1;
+  for(int i=0;i<strlen(v);i++){
+    if(isdigit(v[i])==0){
+      if(isalpha(v[i])==0) return 0;
+      else var=2;
+    }
+  }
+  return var;
+}
+
+void copyMot(const char* src, char* dest, int dep, int fin){
+  int l=0;
+  if(dep>fin || strlen(src)<=fin) return;
+  for(int i=dep;i<=fin;i++){
+    //printf("%c",src[i]);
+    dest[l]=src[i];
+    l++;
+  }  
+  dest[fin+1]='\0';
+  //printf("\n%s\n",dest);
+}
+
 /*
   Ajoute un cellString à une struct expression.
   Si fin>0, l'ajoute à la fin, sinon au début.
 */
-void add_mot(exp* exp, cellString* c,int fin){
+void add_mot(exp* exp, char* mot,int fin){
   if(exp==NULL) return;
+  cellString* c=init_cellString(mot,est_variable_entier(mot));
+  if(c==NULL) return;
   if(nullouvide_cellString(c)!=0) return;
 
   if(exp->premier==NULL){
@@ -81,8 +146,8 @@ void add_mot(exp* exp, cellString* c,int fin){
     return;
   }
   if(fin>0){
-    (exp->dernier)->suivant=c;
     c->precedent=(exp->dernier);
+    (exp->dernier)->suivant=c;    
     exp->dernier=c;
     return;
   }
@@ -91,38 +156,73 @@ void add_mot(exp* exp, cellString* c,int fin){
   exp->premier=c;
 }
 
-/*
-  Vérifie si la chaîne de caractère est éligible d'être le nom d'une variable ou un entier.
-  Retourne 2 si elle contient des lettres ET des chiffres, 1 si elle contient que des chiffres donc elle est un entier, 0 sinon.
-*/
-int est_variable_entier(char* v){
-  if(v==NULL || strlen(v)==0) return 0;
-  int var=2;
-  for(int i=0;i<strlen(v);i++){
-    if(isdigit(v[i])==0){
-      if(isalpha(v[i])==0) return 0;
-      else var=1;
+int parse_line(char* readLine, char** putLine) {
+  int len=strlen(readLine);
+  int l=0;
+  int nbMots=0;
+  for(int i=0;i<len;i++){
+    char c=readLine[i];
+    if(isblank(c)){
+      if(l>0){	
+	putLine[nbMots]=malloc(sizeof(char)*(l+1));
+	if(putLine[nbMots]==NULL){
+	  perror("parse_line/malloc\n");
+	  return -1;
+	}
+	copyMot(readLine,putLine[nbMots],i-l,i-1);
+	nbMots++;
+	l=0;
+	//printf("%s\n",putLine[nbMots-1]);
+      }      
+    }
+    else{
+      if(c=='=' || est_operation(&c)!=0){
+	if(l>0){	
+	  putLine[nbMots]=malloc(sizeof(char)*(l+1));
+	  if(putLine[nbMots]==NULL){
+	    perror("parse_line/malloc\n");
+	    return -1;
+	  }
+	  copyMot(readLine,putLine[nbMots],i-l,i-1);
+	  nbMots++;
+	  l=0;
+	  //printf("%s\n",putLine[nbMots-1]);
+	}
+	putLine[nbMots]=malloc(sizeof(char)*2);
+	if(putLine[nbMots]==NULL){
+	  perror("parse_line/malloc\n");
+	  return -1;
+	}
+        putLine[nbMots][0]=c;
+	putLine[nbMots][1]='\0';
+	nbMots++;
+	//printf("%s\n",putLine[nbMots-1]);
+      }
+      else l++;
     }
   }
-  return var;
+  if(l>0){
+    putLine[nbMots]=malloc(sizeof(char)*(l+1));
+    if(putLine[nbMots]==NULL){
+      perror("parse_line/malloc\n");
+      return -1;
+    }
+    copyMot(readLine,putLine[nbMots],len-l,len-2);
+    l=0;
+  }
+  //printf("%s : %d\n",putLine[nbMots],nbMots+1);
+  nbMots++;
+  return nbMots;
 }
 
-int est_operation(char* v){
-  if(v==NULL || strlen(v)!=1) return 0;
-  if(*v!='+' && *v!='-' && *v!='*') return 0;
-  else return 1;
-}
 
-void parse_line(char* line) {
-  printf("%s", line);
-}
 
 int main(int argc, char *argv[]) {
   /* code */
   int inputArgPos = search_args_options(argc, argv, "-i");
   int outputArgPos = search_args_options(argc, argv, "-o");
-  // printf("%d\n", inputArgPos);
-  // printf("%d\n", outputArgPos);
+  printf("%d\n", inputArgPos);
+  printf("%d\n", outputArgPos);
 
   assert(inputArgPos + 1 != outputArgPos);
   assert(outputArgPos + 1 != inputArgPos);
@@ -149,20 +249,35 @@ int main(int argc, char *argv[]) {
     outputFile = stdout;
   }
 
-  char line[1024];
-
+  char line[NB_CHAR_LINE];  
+  struct expression* expressions[NB_MAX_LINES];
+  int nbMots;
+  int e=0;
+  
   while (fgets(line, sizeof(line), inputFile)) {
-    parse_line(line);
+    char* mots[NB_MAX_MOTS];
+    nbMots=parse_line(line,mots);
+    if(nbMots==-1){
+      return 1;
+    }
+    expressions[e]=init_exp();
+    for(int i=0;i<nbMots;i++){
+      //printf("mot : %s\n",mots[i]);      
+      add_mot(expressions[e],mots[i],1);         
+    }
+    printf("Ligne %d :\n",e);
+    print_exp(expressions[e]);   
+    e++;
   }
 
-  // char *listeMots[] = {"556", "lala", "lala55", "la_la55"};
+  char *listeMots[] = {"556", "lala", "lala55", "la_la55"};
   //
-  // for (int i = 0; i < 4; i++) {
-  //   printf("%s\n", listeMots[i]);
-  //   printf("%d\n\n", est_variable_entier(listeMots[i]));
-  // }
-  //
-  // printf("%d\n", est_operation("*"));
+  for (int i = 0; i < 4; i++) {
+    printf("%s\n", listeMots[i]);
+    printf("%d\n\n", est_variable_entier(listeMots[i]));
+  }
+  
+  printf("%d\n", est_operation("*"));
 
   return 0;
 }
