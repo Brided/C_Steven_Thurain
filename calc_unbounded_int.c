8d2306b8eb64c88d5b617729ea7486bfb8369a44
Steven <ungsteven6@gmail.com>
2022-05-09 11:21:45 +0200

    Le calc_unbounded prend en compte "print" et les entiers négatifs.

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -8,6 +8,7 @@
 #define NB_MAX_MOTS 20
 #define NB_MAX_LINES 20
 #define NB_CHAR_LINE 1024
+#define NB_MAX_VARIABLES 1024
 
 /*
   Cherche la chaine de charactère égale à searched. Retourne sa position si une telle chaine existe, -1 sinon.
@@ -44,7 +45,7 @@ typedef struct expression{
 //struct contenant un nom de variable et une valeur associée au nom de variable.
 typedef struct varStore{
   char* nomVar;
-  int valeur;
+  unbounded_int valeur;
 } varStore;
 
 int nullouvide_cellString(cellString* c){
@@ -103,21 +104,26 @@ int est_operation(char* v){
 /*
   Vérifie si la chaîne de caractère est éligible d'être le nom d'une variable ou un entier.
   Retourne:
+  - 5 si "print"
   - 4 si '='
   - 3 si c'est une opération (+,-,*),
   - 2 si elle contient des lettres et potentiellement des chiffres,
-  - 1 si elle contient que des chiffres donc elle est un entier, 
+  - 1 si elle contient que des chiffres donc elle est un entier,
   - 0 pour tout autre.
 */
 int est_variable_entier(char* v){
   if(v==NULL || strlen(v)==0) return 0;
+  if(!strcmp(v,"print")) return 5;
   int op=est_operation(v);
   if(op==1) return 3;
   else if(op==2) return 4;
   int var=1;
   for(int i=0;i<strlen(v);i++){
     if(isdigit(v[i])==0){
-      if(isalpha(v[i])==0) return 0;
+      if (v[i]=='-' && i==0){
+      }else if(isalpha(v[i])==0){
+        return 0;
+      }
       else var=2;
     }
   }
@@ -131,7 +137,7 @@ void copyMot(const char* src, char* dest, int dep, int fin){
     //printf("%c",src[i]);
     dest[l]=src[i];
     l++;
-  }  
+  }
   dest[fin+1]='\0';
   //printf("\n%s\n",dest);
 }
@@ -153,7 +159,7 @@ void add_mot(exp* exp, char* mot,int fin){
   }
   if(fin>0){
     c->precedent=(exp->dernier);
-    (exp->dernier)->suivant=c;    
+    (exp->dernier)->suivant=c;
     exp->dernier=c;
     return;
   }
@@ -168,45 +174,49 @@ int parse_line(char* readLine, char** putLine) {
   int nbMots=0;
   for(int i=0;i<len;i++){
     char c=readLine[i];
+    char d=readLine[i+1];
+
     if(isblank(c)){
-      if(l>0){	
-	putLine[nbMots]=malloc(sizeof(char)*(l+1));
-	if(putLine[nbMots]==NULL){
-	  perror("parse_line/malloc\n");
-	  return -1;
-	}
-	copyMot(readLine,putLine[nbMots],i-l,i-1);
-	nbMots++;
-	l=0;
-	//printf("%s\n",putLine[nbMots-1]);
-      }      
+      if(l>0){
+      	putLine[nbMots]=malloc(sizeof(char)*(l+1));
+      	if(putLine[nbMots]==NULL){
+      	  perror("parse_line/malloc\n");
+      	  return -1;
+      	}
+      	copyMot(readLine,putLine[nbMots],i-l,i-1);
+      	nbMots++;
+      	l=0;
+      	printf("var_name: %s\n",putLine[nbMots-1]);
+      }
     }
+
     else{
-      if(c=='=' || est_operation(&c)!=0){
-	if(l>0){	
-	  putLine[nbMots]=malloc(sizeof(char)*(l+1));
-	  if(putLine[nbMots]==NULL){
-	    perror("parse_line/malloc\n");
-	    return -1;
-	  }
-	  copyMot(readLine,putLine[nbMots],i-l,i-1);
-	  nbMots++;
-	  l=0;
-	  //printf("%s\n",putLine[nbMots-1]);
-	}
-	putLine[nbMots]=malloc(sizeof(char)*2);
-	if(putLine[nbMots]==NULL){
-	  perror("parse_line/malloc\n");
-	  return -1;
-	}
+      if(c=='=' || (c=='-' && est_operation(&d) != 0) || est_operation(&c)!=0){
+      	if(l>0){
+      	  putLine[nbMots]=malloc(sizeof(char)*(l+1));
+      	  if(putLine[nbMots]==NULL){
+      	    perror("parse_line/malloc\n");
+      	    return -1;
+      	  }
+      	  copyMot(readLine,putLine[nbMots],i-l,i-1);
+      	  nbMots++;
+      	  l=0;
+      	  //printf("%s\n",putLine[nbMots-1]);
+      	}
+      	putLine[nbMots]=malloc(sizeof(char)*2);
+      	if(putLine[nbMots]==NULL){
+      	  perror("parse_line/malloc\n");
+      	  return -1;
+      	}
         putLine[nbMots][0]=c;
-	putLine[nbMots][1]='\0';
-	nbMots++;
-	//printf("%s\n",putLine[nbMots-1]);
+      	putLine[nbMots][1]='\0';
+      	nbMots++;
+      	//printf("%s\n",putLine[nbMots-1]);
       }
       else l++;
     }
   }
+
   if(l>0){
     putLine[nbMots]=malloc(sizeof(char)*(l+1));
     if(putLine[nbMots]==NULL){
@@ -221,14 +231,16 @@ int parse_line(char* readLine, char** putLine) {
   return nbMots;
 }
 
+void exec_exp(exp* exp) {
 
+}
 
 int main(int argc, char *argv[]) {
   /* code */
   int inputArgPos = search_args_options(argc, argv, "-i");
   int outputArgPos = search_args_options(argc, argv, "-o");
-  printf("%d\n", inputArgPos);
-  printf("%d\n", outputArgPos);
+  // printf("%d\n", inputArgPos);
+  // printf("%d\n", outputArgPos);
 
   assert(inputArgPos + 1 != outputArgPos);
   assert(outputArgPos + 1 != inputArgPos);
@@ -255,11 +267,12 @@ int main(int argc, char *argv[]) {
     outputFile = stdout;
   }
 
-  char line[NB_CHAR_LINE];  
-  struct expression* expressions[NB_MAX_LINES];
+  varStore* pile[NB_MAX_VARIABLES];
+  char line[NB_CHAR_LINE];
+  exp* expressions[NB_MAX_LINES];
   int nbMots;
   int e=0;
-  
+
   while (fgets(line, sizeof(line), inputFile)) {
     char* mots[NB_MAX_MOTS];
     nbMots=parse_line(line,mots);
@@ -268,22 +281,21 @@ int main(int argc, char *argv[]) {
     }
     expressions[e]=init_exp();
     for(int i=0;i<nbMots;i++){
-      //printf("mot : %s\n",mots[i]);      
-      add_mot(expressions[e],mots[i],1);         
+      // printf("mot : %s\n",mots[i]);
+      add_mot(expressions[e],mots[i],1);
     }
     printf("Ligne %d :\n",e);
-    print_exp(expressions[e]);   
+    print_exp(expressions[e]);
     e++;
   }
 
-  char *listeMots[] = {"556", "lala", "lala55", "la_la55"};
+  // char *listeMots[] = {"556", "lala", "lala55", "la_la55"};
+  // for (int i = 0; i < 4; i++) {
+  //   printf("%s\n", listeMots[i]);
+  //   printf("%d\n\n", est_variable_entier(listeMots[i]));
+  // }
   //
-  for (int i = 0; i < 4; i++) {
-    printf("%s\n", listeMots[i]);
-    printf("%d\n\n", est_variable_entier(listeMots[i]));
-  }
-  
-  printf("%d\n", est_operation("*"));
+  // printf("%d\n", est_operation("*"));
 
   fclose(inputFile);
   fclose(outputFile);
